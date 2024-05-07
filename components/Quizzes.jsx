5bad2e6e723de1373b18e296b8256b87c103cb1b
Andrew Hutcheson <andrewhutch10@gmail.com>
2024-05-07 07:31:41 -0500

    change quizzes api handling

--- components/Quizzes.jsx
+++ components/Quizzes.jsx
@@ -4,28 +4,24 @@ import {useSession } from "next-auth/react";
 
 const Quizzes = () => {
   const { data: session } = useSession();
-    const [quizzes, setQuizzes] = useState([]);
-    const [refreshQuizzes, setRefreshQuizzes] = useState(0);
-    /* console.log(quizzes, session) */
-    const fetchQuizzes = async () => {
-      const response = await fetch('/api/quiz');
-      const data = await response.json();
-      setQuizzes(data)
-      //setQuizzes(data.filter( quiz => quiz.creator == session?.user.id));
-      /* setQuizzes(data) */
-      console.log(quizzes);
-  }
-
+  const [quizzes, setQuizzes] = useState([]);
+  const [refreshQuizzes, setRefreshQuizzes] = useState(0);
     useEffect(() => {
-    
-    fetchQuizzes();
+      async function fetchQuizzes() {
+        const response = await fetch('/api/quiz');
+        const json = await response.json();
+        setQuizzes(json.filter( quiz => quiz.creator == session?.user.id));
+        /* setQuizzes(prev => console.log("this is prev", prev)) */
+      } 
+      fetchQuizzes();
+
     }, [refreshQuizzes]);
   return (
     <section className='quizzes'>
         <h2 className='your-quizzes-title'> Your Quizzes</h2>
         <div className='your-quizzes'>
           {/* <h3>{quizzes.map(val => val?.title)}</h3> */}
-          {quizzes.map(val => <QuizCard setRefreshQuizzes={setRefreshQuizzes} key={val?._id} quiz_id={val?._id} creator={val?.creator} title={val?.title} quiz={val?.quiz}/>)}
+          {quizzes && quizzes.map(val => <QuizCard setRefreshQuizzes={setRefreshQuizzes} key={val?._id} quiz_id={val?._id} creator={val?.creator} title={val?.title} quiz={val?.quiz}/>)}
         </div>
     </section>
   )
