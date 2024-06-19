045f1d7e7205cde18bfc9cb9cad15eae98a52f7f
Andrew Hutcheson <andrewhutch10@gmail.com>
2024-06-19 13:32:47 -0500

    change api route to no-store

--- app/api/quiz/route.js
+++ app/api/quiz/route.js
@@ -6,7 +6,16 @@ import Quiz from '@models/quiz'
 export const GET = async (req, res) => {
     /* console.log("this is the session", session) */
     try{
-    
+        res.setHeader('Cache-Control', 'no-store')
+
+       /*  if (apiRes.status === 200) {    
+            // "Cache-Control"-header needed for production to avoid Vercel's default 
+            // response caching mechanism        
+            res.setHeader(
+              "Cache-Control",
+              "no-cache, no-store, max-age=0, must-revalidate"
+            );
+        } */
         /* const session = await getServerSession(handler);
         console.log("This is the session", session) */
         await connectToDB();
--- components/Quizzes.jsx
+++ components/Quizzes.jsx
@@ -6,11 +6,13 @@ const Quizzes = () => {
   const { data: session } = useSession();
   const [quizzes, setQuizzes] = useState([]);
   const [refreshQuizzes, setRefreshQuizzes] = useState(0);
+  const user_id = session?.user.id
     useEffect(() => {
       async function fetchQuizzes() {
         const response = await fetch('/api/quiz');
         const json = await response.json();
-        setQuizzes(json.filter( quiz => quiz.creator == session?.user.id));
+        console.log(quizzes)
+        setQuizzes(json.filter( quiz => quiz.creator == user_id));
         /* setQuizzes(prev => console.log("this is prev", prev)) */
       } 
       fetchQuizzes();
